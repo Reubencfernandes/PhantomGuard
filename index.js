bbf4abc157f252e3f7f3398e45544b8051cfb191
Joshua Lewis <Darkwater409@gmail.com>
2019-09-02 23:37:09 -0700

    added files via upload

--- index.js
+++ index.js
@@ -7,10 +7,61 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+client.on('ready', () => {
+    // List servers the bot is connected to
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log(" - " + guild.name)
+
+        // List all channels
+        guild.channels.forEach((channel) => {
+            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+        })
+    })
+})
+
+client.on('ready', () => {
+    var generalChannel = client.channels.get("383746228443152384") // Replace with known channel ID
+    generalChannel.send("I have arrived!")  
+})
+
+client.on('ready', () => {
+    var generalChannel = client.channels.get("383746228443152384") // Replace with known channel ID
+  
+    // Provide a path to a local file
+    const localFileAttachment = new Discord.Attachment('Banner.PNG')
+    generalChannel.send(localFileAttachment)
+
+})
+
+client.on('ready', () => {
+    // Set bot status to: "Playing wth Javascript"
+    client.user.setActivity("with Javascript")
+
+    // Alternatively, you can set the activity to any of the following:
+    // PLAYING, STREAMING, LISTENING, WATCHING
+    // For example:
+    // client.user.setActivity("TV", {type: "WATCHING"})
+})
+
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content === '!ping') {
     msg.reply('Pong!')
   }
+})
+
+client.on('message', (receivedMessage) => {
+    // Prevent bot from responding to its own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+    
+    // Check if the bot's user was tagged in the message
+    if (receivedMessage.content.includes(client.user.toString())) {
+        // Send acknowledgement message
+        receivedMessage.channel.send("Message received from " +
+            receivedMessage.author.toString() + ": " + receivedMessage.content)
+    }
 })
 
 client.on('message', message => {
@@ -33,4 +84,24 @@ client.on('message', message => {
       .catch(error => message.reply(`Sorry, an error occured.`))
   }
 })
+
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === '!join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+});
+
 client.login(process.env.BOT_TOKEN)

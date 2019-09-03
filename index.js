9908b02d0a75d3238d0232787a0fbd269843ad81
Joshua Lewis <Darkwater409@gmail.com>
2019-09-03 03:16:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('ready', () => {
-    // List servers the bot is connected to
+client.on('ready', () => {    
     console.log("Servers:")
     client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name)
-
-        // List all channels
+        console.log(" - " + guild.name)        
         guild.channels.forEach((channel) => {
             console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
         })
@@ -21,27 +18,18 @@ client.on('ready', () => {
 })
 
 client.on('ready', () => {
-    var generalChannel = client.channels.get("383746228443152384") // Replace with known channel ID
+    var generalChannel = client.channels.get("383746228443152384")
     generalChannel.send("I have arrived!")  
 })
 
 client.on('ready', () => {
-    var generalChannel = client.channels.get("383746228443152384") // Replace with known channel ID
-  
-    // Provide a path to a local file
+    var generalChannel = client.channels.get("383746228443152384")      
     const localFileAttachment = new Discord.Attachment('Banner.PNG')
     generalChannel.send(localFileAttachment)
-
 })
 
-client.on('ready', () => {
-    // Set bot status to: "Playing wth Javascript"
+client.on('ready', () => {    
     client.user.setActivity("with Javascript", {type: "PLAYING"})
-
-    // Alternatively, you can set the activity to any of the following:
-    // PLAYING, STREAMING, LISTENING, WATCHING
-    // For example:
-    // client.user.setActivity("TV", {type: "WATCHING"})
 })
 
 client.on('message', msg => {
@@ -52,24 +40,20 @@ client.on('message', msg => {
   }
 })
 
-client.on('message', (receivedMessage) => {
-    // Prevent bot from responding to its own messages
+client.on('message', (receivedMessage) => {    
     if (receivedMessage.author == client.user) {
         return
-	}
+	} 
     
-    // Check if the bot's user was tagged in the message
     if (receivedMessage.content.includes(client.user.toString())) {
-        // Send acknowledgement message
-        receivedMessage.channel.send("Message received from " +
-            receivedMessage.author.toString() + ": " + receivedMessage.content)
+        receivedMessage.channel.send("My Prefix is !" )
     }
 })
 
 client.on('message', message => {
   if (message.content.startsWith('!kick')) {
     const member = message.mentions.members.first()
-
+	
     if (!member) {
       return message.reply(
         `Who are you trying to kick? You must mention a user.`
@@ -86,24 +70,5 @@ client.on('message', message => {
       .catch(error => message.reply(`Sorry, an error occured.`))
   }
 })
-
-client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
-
-  if (message.content === '!join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
 
 client.login(process.env.BOT_TOKEN)

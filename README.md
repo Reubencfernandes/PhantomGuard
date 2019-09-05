847957381906bca470669224528e02b8975b2779
Joshua Lewis <Darkwater409@gmail.com>
2019-09-05 04:33:47 -0700

    updating files

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # PhantomGuard
- Private Moderation bot for PhantomDEV
+ The official moderation bot of PhantomDEV
 
 ## Dependencies
->- Discord.js
->- dotenv
+>- Discord.js 11.5.1
+>- eslint 6.3.0
 
 ## devDependencies
->- nodemon
+>- nodemon 1.19.2
 
 ## Support Discord
 **PhantomDEV**: https://auttaja.io/PhantomDEV
--- index.js
+++ index.js
@@ -1,74 +1,113 @@
-require('dotenv').config()
-const fs = require('fs')
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-client.on('ready', () => {    
-    console.log("Servers:")
-    client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name)        
-        guild.channels.forEach((channel) => {
-            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
-        })
-    })
-})
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.on('ready', () => {
-    var generalChannel = client.channels.get("383746228443152384")
-    generalChannel.send("I have arrived!")  
-})
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
-client.on('ready', () => {
-    var generalChannel = client.channels.get("383746228443152384")      
-    const localFileAttachment = new Discord.Attachment('Banner.PNG')
-    generalChannel.send(localFileAttachment)
-})
-
-client.on('ready', () => {    
-    client.user.setActivity("with Javascript", {type: "PLAYING"})
-})
-
-client.on('message', msg => {
-  if (msg.content === '!ping') {
-    msg.reply('Pong!')
-  } else if (msg.content === '!beep') {
-	  msg.reply('Boop!')
-  }
-})
-
-client.on('message', (receivedMessage) => {    
-    if (receivedMessage.author == client.user) {
-        return
-	} 
-    
-    if (receivedMessage.content.includes(client.user.toString())) {
-        receivedMessage.channel.send("My Prefix is !" )
-    }
-})
+// `client.on('...')` events and such below this point
 
 client.on('message', message => {
-  if (message.content.startsWith('!kick')) {
-    const member = message.mentions.members.first()
-	
-    if (!member) {
-      return message.reply(
-        `Who are you trying to kick? You must mention a user.`
-      )
-    }
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    if (!member.kickable) {
-      return message.reply(`I can't kick this user. Sorry!`)
-    }
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
 
-    return member
-      .kick()
-      .then(() => message.reply(`${member.user.tag} was kicked.`))
-      .catch(error => message.reply(`Sorry, an error occured.`))
-  }
-})
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	} else if (command === 'server') {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	} else if (command === 'user-info') {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	} else if (command === 'info') {
+		if (!args.length) {
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		} else if (args[0] === 'foo') {
+			return message.channel.send('bar');
+		}
 
-client.login(process.env.BOT_TOKEN)
+		message.channel.send(`First argument: ${args[0]}`);
+	} else if (command === 'kick') {
+		if (!message.mentions.users.size) {
+			return message.reply('you need to tag a user in order to kick them!');
+		}
+
+		const taggedUser = message.mentions.users.first();
+
+		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+	} else if (command === 'avatar') {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: ${user.displayAvatarURL}`;
+		});
+
+		message.channel.send(avatarList);
+	} else if (command === 'prune') {
+		const amount = parseInt(args[0]) + 1;
+
+		if (isNaN(amount)) {
+			return message.reply('that doesn\'t seem to be a valid number.');
+		} else if (amount <= 1 || amount > 100) {
+			return message.reply('you need to input a number between 1 and 99.');
+		}
+
+		message.channel.bulkDelete(amount, true).catch(err => {
+			console.error(err);
+			message.channel.send('there was an error trying to prune messages in this channel!');
+		});
+	}
+});
+
+client.on("ready", function(){
+    console.log(`the client becomes ready to start`);
+	console.log(`I am ready! Logged in as ${client.user.tag}!`);
+	console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+
+  	client.user.setActivity("with Javascript", {type: "PLAYING"});
+	client.generateInvite(['SEND_MESSAGES', 'MANAGE_GUILD', 'MENTION_EVERYONE'])
+	.then(link => {
+		console.log(`Generated bot invite link: ${link}`);
+		inviteLink = link;
+	});
+});
+
+client.on("guildCreate", function(guild){
+    console.log(`the client joins a guild`);
+});
+
+client.on("guildDelete", function(guild){
+    console.log(`the client deleted/left a guild`);
+});
+
+client.on("guildMemberAdd", (member) => {
+  console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
+  member.guild.channels.find(c => c.name === "welcome").send(`"${member.user.username}" has joined this server`);
+});
+
+client.on("guildMemberRemove", function(member){
+    console.log(`a member leaves a guild, or is kicked: ${member.tag}`);
+});
+
+client.on("guildBanAdd", function(guild, user){
+    console.log(`a member is banned from a guild`);
+});
+
+client.on("guildBanRemove", function(guild, user){
+    console.log(`a member is unbanned from a guild`);
+});
+
+
+
+client.login(token);

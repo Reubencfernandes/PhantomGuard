cf6f45d29e5b8b8b7b1b74abc0d313da115fe7ba
reuben [gco360] <31587917+gco360@users.noreply.github.com>
2019-09-05 18:49:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
-
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
-
-// `client.on('...')` events and such below this point
-
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 

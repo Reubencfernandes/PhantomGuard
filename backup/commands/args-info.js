445e99268683e5e0e7562c8b6156bd74d0eceacd
Joshua Lewis <Darkwater409@gmail.com>
2019-09-08 09:01:54 -0700

    Create args-info.js

---
+++ backup/commands/args-info.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const prefix="!";
+exports.argsinfo(client,message)=> {
+if(message.content.startsWith(prefix+"argslength")
+   {
+       let args = message.content.split(' ').slice(1);
+       let text = args.join(' ');
+	message.channel.send(text.length);
+
+   }
+
+}

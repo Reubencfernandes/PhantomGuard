80932783cdaabab643bba8a7b0c7f7ff5f554578
Joshua Lewis <Darkwater409@gmail.com>
2019-09-08 04:18:04 -0700

    Add files via upload

--- commands/args-info.js
+++ commands/args-info.js
@@ -1,13 +1,12 @@
-const Discord = require("discord.js");
-const prefix="!";
-exports.argsinfo(client,message)=> {
-if(message.content.startsWith(prefix+"argslength")
-   {
-       let args = message.content.split(' ').slice(1);
-       let text = args.join(' ');
-	message.channel.send(text.length);
+module.exports = {
+	name: 'args-info',
+	description: 'Information about the arguments provided.',
+	args: true,
+	execute(message, args) {
+		if (args[0] === 'foo') {
+			return message.channel.send('bar');
+		}
 
-   }
-
-}
-
+		message.channel.send(`First argument: ${args[0]}`);
+	},
+};

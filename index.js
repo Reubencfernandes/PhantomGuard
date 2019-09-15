897d9a2cf9411cd406d2f10fcc7efa1bdd0c899f
Joshua Lewis <Darkwater409@gmail.com>
2019-09-14 21:28:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,18 @@ for (const file of commandFiles) {
 
 const cooldowns = new Discord.Collection();
 
-client.once('ready', () => {
-	console.log('Ready!');
+client.on("ready", function(){
+	console.log(`the client becomes ready to start`);
+	console.log(`I am ready! Logged in as ${client.user.tag}!`);
+	console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+
+			client.user.setActivity("with Node.JS");
+			client.generateInvite(['SEND_MESSAGES', 'MANAGE_GUILD', 'MENTION_EVERYONE'])
+			
+			.then(link => {
+				console.log(`Generated bot invite link: ${link}`);
+				inviteLink = link;
+})
 });
 
 const applyText = (canvas, text) => {
